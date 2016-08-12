8bfe779cf3b76a1b9b9a2060c0835e05f21c871f
kenichinakahara <nakaharak29@nakaharak29.local>
2016-08-12 17:24:48 +0900

    Lesson終了

---
+++ Instagram/Const.swift
@@ -0,0 +1,14 @@
+//
+//  Const.swift
+//  Instagram
+//
+//  Created by nakaharak29 on 8/12/16.
+//  Copyright © 2016 kenic. All rights reserved.
+//
+
+import Foundation
+
+struct CommonConst {
+    static let PostPATH = "posts"
+    static let DisplayNameKey = "DISPLAYNAME"
+}
---
+++ Instagram/Instagram-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Instagram-Bridging-Header.h
+//  Instagram
+//
+//  Created by nakaharak29 on 8/12/16.
+//  Copyright © 2016 kenic. All rights reserved.
+//
+
+#ifndef Instagram_Bridging_Header_h
+#define Instagram_Bridging_Header_h
+
+#import <AdobeCreativeSDKCore/AdobeCreativeSDKCore.h>
+#import <AdobeCreativeSDKImage/AdobeCreativeSDKImage.h>
+
+#endif /* Instagram_Bridging_Header_h */
